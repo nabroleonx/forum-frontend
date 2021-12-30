fd64d79e2ea83bb71a71337392d618f5e7e15411
nabroleonx <nabroleonx@gmail.com>
2021-12-30 20:41:08 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./index.css";
+import { ClockIcon } from "@heroicons/react/outline";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen">
+      <div className="inline-flex items-center text-cyan-600 border py-1 px-2 border-transparent bg-cyan-50 rounded-md">
+        <ClockIcon className="h-8 w-8 text-cyan-600 pr-1" />{" "}
+        <span className="font-medium">2 mins</span>
+      </div>
     </div>
   );
 }
