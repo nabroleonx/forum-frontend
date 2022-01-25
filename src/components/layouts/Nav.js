a2ef3c3787ca871f5d715df773332942ac7915c1
nabroleonx <nabroleonx@gmail.com>
2022-01-25 21:26:42 +0300

    Add conditional rendering to nav bar

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, Popover, Transition } from "@headlessui/react";
 import { SearchIcon } from "@heroicons/react/solid";
+import { useSelector } from "react-redux";
 import {
   BellIcon,
   FireIcon,
@@ -37,6 +38,7 @@ function classNames(...classes) {
 
 export default function Nav() {
   const [modal, setModal] = useState(false);
+  const { token } = useSelector((state) => state.auth);
   return (
     <>
       <Popover
@@ -108,69 +110,75 @@ export default function Nav() {
                   </a>
 
                   {/* Profile dropdown */}
-                  <Menu as="div" className="flex-shrink-0 relative ml-5">
-                    <div>
-                      <Menu.Button className="bg-white rounded-full flex focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src={user.imageUrl}
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <Link to={item.href}>
-                                <p
-                                  className={classNames(
-                                    active ? "bg-gray-100" : "",
-                                    "block py-2 px-4 text-sm text-gray-700"
-                                  )}
-                                >
-                                  {item.name}
-                                </p>
-                              </Link>
-                            )}
+                  {token && (
+                    <Menu as="div" className="flex-shrink-0 relative ml-5">
+                      <div>
+                        <Menu.Button className="bg-white rounded-full flex focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-8 w-8 rounded-full"
+                            src={user.imageUrl}
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <Link to={item.href}>
+                                  <p
+                                    className={classNames(
+                                      active ? "bg-gray-100" : "",
+                                      "block py-2 px-4 text-sm text-gray-700"
+                                    )}
+                                  >
+                                    {item.name}
+                                  </p>
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+
+                          <Menu.Item>
+                            <button
+                              className="block py-2 px-4 text-sm text-gray-700"
+                              onClick={() => setModal(true)}
+                            >
+                              {" "}
+                              Logout
+                            </button>
                           </Menu.Item>
-                        ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  )}
+                  {!token && (
+                    <>
+                      <Link
+                        to="/login"
+                        className="ml-6 inline-flex items-center px-4 py-2 border border-gray-400 text-sm font-medium rounded-md shadow-sm text-gray-900 hover:text-white hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+                      >
+                        Login
+                      </Link>
 
-                        <Menu.Item>
-                          <button
-                            className="block py-2 px-4 text-sm text-gray-700"
-                            onClick={() => setModal(true)}
-                          >
-                            {" "}
-                            Logout
-                          </button>
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                  <Link
-                    to="/login"
-                    className="ml-6 inline-flex items-center px-4 py-2 border border-gray-400 text-sm font-medium rounded-md shadow-sm text-gray-900 hover:text-white hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                  >
-                    Login
-                  </Link>
-
-                  <Link
-                    to="/register"
-                    className="ml-6 inline-flex items-center px-5 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                  >
-                    Register
-                  </Link>
+                      <Link
+                        to="/register"
+                        className="ml-6 inline-flex items-center px-5 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+                      >
+                        Register
+                      </Link>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
@@ -193,64 +201,67 @@ export default function Nav() {
                   </Link>
                 ))}
               </div>
-              <div className="border-t border-gray-200 pt-4">
-                <div className="max-w-3xl mx-auto px-4 flex items-center sm:px-6">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={user.imageUrl}
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">
-                      {user.name}
+              {token && (
+                <div className="border-t border-gray-200 pt-4">
+                  <div className="max-w-3xl mx-auto px-4 flex items-center sm:px-6">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src={user.imageUrl}
+                        alt=""
+                      />
                     </div>
-                    <div className="text-sm font-medium text-gray-500">
-                      {user.email}
+                    <div className="ml-3">
+                      <div className="text-base font-medium text-gray-800">
+                        {user.name}
+                      </div>
+                      <div className="text-sm font-medium text-gray-500">
+                        {user.email}
+                      </div>
                     </div>
-                  </div>
-                  <button
-                    type="button"
-                    className="ml-auto flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mt-3 max-w-3xl mx-auto px-2 space-y-1 sm:px-4">
-                  {userNavigation.map((item) => (
                     <button
-                      key={item.name}
-                      className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
+                      type="button"
+                      className="ml-auto flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
                     >
-                      {item.name}
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
-                  ))}
-                  <button
-                    to="/register"
-                    className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
-                    onClick={() => setModal(true)}
-                  >
-                    Logout
-                  </button>
+                  </div>
+                  <div className="mt-3 max-w-3xl mx-auto px-2 space-y-1 sm:px-4">
+                    {userNavigation.map((item) => (
+                      <button
+                        key={item.name}
+                        className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </button>
+                    ))}
+                    <button
+                      to="/register"
+                      className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
+                      onClick={() => setModal(true)}
+                    >
+                      Logout
+                    </button>
+                  </div>
                 </div>
-              </div>
-
-              <div className="mt-6 max-w-3xl mx-auto px-4 sm:px-6 space-y-2">
-                <Link
-                  to="/login"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-gray-400 text-base font-medium rounded-md shadow-sm text-gray-500 hover:text-white hover:bg-teal-600"
-                >
-                  Login
-                </Link>
-                <Link
-                  to="/register"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-teal-600 hover:bg-teal-700"
-                >
-                  Register
-                </Link>
-              </div>
+              )}
+              {!token && (
+                <div className="mt-6 max-w-3xl mx-auto px-4 sm:px-6 space-y-2">
+                  <Link
+                    to="/login"
+                    className="w-full flex items-center justify-center px-4 py-2 border border-gray-400 text-base font-medium rounded-md shadow-sm text-gray-500 hover:text-white hover:bg-teal-600"
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to="/register"
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-teal-600 hover:bg-teal-700"
+                  >
+                    Register
+                  </Link>
+                </div>
+              )}
             </Popover.Panel>
           </>
         )}
