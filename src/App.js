cbb3d59f104fa452688441150cc61eb1d3ebbc55
Hannah <66206865+earthcomfy@users.noreply.github.com>
2022-01-30 18:49:54 +0300

    Add private route component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/accounts/Register";
 import Landing from "./components/layouts/Landing";
 import Main from "./components/layouts/Main";
 import ErrorMessage from "./components/layouts/ErrorMessage";
+import WithPrivateRoute from "./utils/WithPrivateRoute";
 
 function App() {
   return (
@@ -16,7 +17,15 @@ function App() {
         <Route index element={<Landing />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/main" element={<Main />} />
+        <Route
+          exact
+          path="/main"
+          element={
+            <WithPrivateRoute>
+              <Main />
+            </WithPrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
