6d4d602c3eae280577b0dfcd491d737b7b091bde
Hannah <66206865+earthcomfy@users.noreply.github.com>
2022-03-18 18:26:49 +0300

    Fix margin for bulllet and numbered list

--- src/components/question/QuestionCreate.js
+++ src/components/question/QuestionCreate.js
@@ -1,11 +1,7 @@
 // TAke a look at @stackoverflow/stacks-editor package, its the one stackoverflow uses;
 
 import React, { useState } from "react";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import parse from "html-react-parser";
 
 export default function QuestionCreate() {
-  const [questionBody, setQuestionBody] = useState("");
   return <div>hello</div>;
 }
