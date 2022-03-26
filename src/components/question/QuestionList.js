2aa0818a482c47411d89737c648df72af0c43d7c
kazumaneet <mekbibtare@gmail.com>
2022-03-26 22:04:37 +0300

    display the saved questions

--- src/components/question/QuestionList.js
+++ src/components/question/QuestionList.js
@@ -1,5 +1,5 @@
-import { useEffect} from "react"
-import { useSelector,useDispatch} from "react-redux";
+import { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux";
 import { getQuestions } from "../../redux/actions/questions";
 export default function QuestionList() {
 
@@ -11,7 +11,33 @@ export default function QuestionList() {
 
   console.log(questions)
   return (
-  <>
-    
-  </>)
+    <>
+      <ul className="space-y-4 list-none">
+        {questions.map((question) => (
+          <li
+            key={question.id}
+            className="bg-white px-4 py-6 shadow sm:p-6 sm:rounded-lg">
+            <div>
+              <div className="flex space-x-3">
+                <div className="min-w-0 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    {question.author.name}
+                  </p>
+                </div>
+              </div>
+              <h2
+                id={"question-title-" + question.id}
+                className="mt-4 text-base font-medium text-gray-900"
+              >
+                {question.title}
+              </h2>
+            </div>
+            <div
+              className="mt-2 text-sm text-gray-700 space-y-4"
+              dangerouslySetInnerHTML={{ __html: question.body }}
+            />
+          </li>
+        ))}
+      </ul>
+    </>)
 }
--- src/redux/reducers/questions.js
+++ src/redux/reducers/questions.js
@@ -9,7 +9,7 @@ import {
 
 const initialState = {
   isLoading: false,
-  questions:null,
+  questions:[],
   question: null,
   questionBody: null,
 };
