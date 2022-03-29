2bd083ee4f068ac08a1a791fe32520e320d4c757
nabroleonx <nabroleonx@gmail.com>
2022-03-29 12:51:34 +0300

    Add image upload feature with imgbb api

--- src/components/question/Editor.js
+++ src/components/question/Editor.js
@@ -20,6 +20,23 @@ export default function Editor(props) {
     []
   );
 
+  const handleImageUpload = (file) =>
+    new Promise((resolve, reject) => {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      fetch(
+        "https://api.imgbb.com/1/upload?key=7fe333bafed6f7fc10c51c34bf795700",
+        {
+          method: "POST",
+          body: formData,
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => resolve(result.data.url))
+        .catch(() => reject(new Error("Upload failed")));
+    });
+
   return (
     <RichTextEditor
       controls={[
@@ -33,6 +50,7 @@ export default function Editor(props) {
       modules={modules}
       value={value}
       onChange={onChange}
+      onImageUpload={handleImageUpload}
     />
   );
 }
